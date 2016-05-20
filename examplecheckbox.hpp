c2b422a559024fdb4f587bfc8509d3d08b39305d
Tóth Ferenc Oszkár <tothfo@gmail.com>
2016-05-20 23:41:33 +0200

    Add files via upload

--- examplecheckbox.hpp
+++ examplecheckbox.hpp
@@ -5,11 +5,14 @@
 #include "widgets.hpp"
 
 class ExampleCheckBox : public Widget {
-protected:
-    bool _checked;
 public:
+    char kar;
+    bool _checked;
+    bool valtas;
+    bool aktiv;
+
     ExampleCheckBox(int x, int y, int sx, int sy);
-    virtual void draw() const ;
+    virtual void draw() const;
     virtual void handle(genv::event ev);
     virtual bool is_checked() const ;
     virtual void action(genv::event ev);
